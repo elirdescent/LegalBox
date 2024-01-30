7efbf736c965c30dc2a464eaf55bfd806dcd4ee1
elirdescent <elshanielird@gmail.com>
2024-01-30 18:54:03 +0100

    Added home page features section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,10 @@
     <title>Home</title>
     @vite('resources/css/app.css')
 </head>
-<body>
+<body class="background-radial-gradient">
 
     <!-- Section: Design Block -->
-<section class="background-radial-gradient mb-40 overflow-hidden">
+<section class=" mb-10 overflow-hidden">
   <style>
   .background-radial-gradient {
     background-color: hsl(180, 41%, 15%);
@@ -119,7 +119,7 @@
   <!-- Navbar -->
 
   <!-- Jumbotron -->
-  <div class="px-6 py-12 text-center md:px-12 lg:py-24 lg:text-left">
+  <div class=" text-center md:px-12 lg:py-24 lg:text-left">
     <div class="w-100 mx-auto text-neutral-800 sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
       <div class="grid items-center gap-12 lg:grid-cols-2">
         <div class="mt-12 lg:mt-0" style="z-index: 10">
@@ -246,6 +246,186 @@
   
   <!-- Jumbotron -->
 </section>
+
+<div class="container my-12 mx-auto  mt-0">
+  <!-- Features Blockk -->
+  <section class="mb-32">
+    <style>
+      
+    </style>
+
+    <div class=" text-center text-white lg:h-[400px] h-[300px] lg:pt-[80px] pt-[55px]">
+      <h2 class="mb-12 text-cyan-400 text-center text-4xl font-bold">Features</h2>
+    </div>
+
+    <div class="grid px-6 md:px-12 lg:grid-cols-3 xl:px-32" style="margin-top: -200px">
+      <div class="p-0 py-12">
+        <div
+          class="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 lg:rounded-tr-none lg:rounded-br-none">
+          <div class="border-b-2 border-neutral-100 border-opacity-100 p-6 text-center dark:border-opacity-10">
+            <p class="mb-4 text-sm uppercase">
+              <strong>Basic</strong>
+            </p>
+            <h3 class="mb-6 text-3xl">
+              <strong>$ 129</strong>
+              <small class="text-base text-neutral-500 dark:text-neutral-300">/year</small>
+            </h3>
+
+            <button type="button"
+              class="inline-block w-full rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+              data-te-ripple-init data-te-ripple-color="light">
+              Buy
+            </button>
+          </div>
+          <div class="p-6">
+            <ol class="list-inside">
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                updates
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                repository
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                installation
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div
+          class="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+          style="z-index: 1">
+          <div class="border-b-2 border-neutral-100 border-opacity-100 p-6 text-center dark:border-opacity-10">
+            <p class="mb-4 text-sm uppercase">
+              <strong>Enterprise</strong>
+            </p>
+            <h3 class="mb-6 text-3xl">
+              <strong>$ 499</strong>
+              <small class="text-base text-neutral-500 dark:text-neutral-300">/year</small>
+            </h3>
+
+            <button type="button"
+              class="inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              data-te-ripple-init data-te-ripple-color="light">
+              Buy
+            </button>
+          </div>
+          <div class="p-6">
+            <ol class="list-inside">
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                updates
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                repository
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                installation
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Code examples
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Premium
+                snippets
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Premium
+                support
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Drag&amp;Drop
+                builder
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+
+      <div class="py-12">
+        <div
+          class="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 lg:rounded-tl-none lg:rounded-bl-none">
+          <div class="border-b-2 border-neutral-100 border-opacity-100 p-6 text-center dark:border-opacity-10">
+            <p class="mb-4 text-sm uppercase">
+              <strong>Advanced</strong>
+            </p>
+            <h3 class="mb-6 text-3xl">
+              <strong>$ 299</strong>
+              <small class="text-base text-neutral-500 dark:text-neutral-300">/year</small>
+            </h3>
+
+            <button type="button"
+              class="inline-block w-full rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+              data-te-ripple-init data-te-ripple-color="light">
+              Buy
+            </button>
+          </div>
+          <div class="p-6">
+            <ol class="list-inside">
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Unlimited
+                updates
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Git
+                repository
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>npm
+                installation
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Code examples
+              </li>
+              <li class="mb-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" class="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>Premium
+                snippets
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Design Block -->
+</div>
 <!-- Footer -->
 <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
   <nav class="grid grid-flow-col gap-4">
