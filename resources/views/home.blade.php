b547bb68207155f91339e7907dddb8bff02c5c62
elirdescent <elshanielird@gmail.com>
2024-02-02 17:31:46 +0100

    Added TailwindCSS Animation plugin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,7 +79,7 @@
     <div class="w-100 mx-auto text-neutral-800 sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
       <div class="grid items-center  gap-12 lg:grid-cols-2">
         <div class=" mt-12 lg:mt-0 px-10 lg:px-0" style="z-index: 10">
-          <img src="{{URL('images/legalboxteal.png')}}" alt="" class="h-20 ">
+          <img src="{{URL('images/legalboxteal.png')}}" alt="" class="h-20 animate-fade-down ">
           <h1
             class="mt-0  mb-12 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl text-[hsl(218,81%,95%)]">
            
