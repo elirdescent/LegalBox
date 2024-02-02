6324dcc458cb71d642a3b57052b6581e3725c83e
elirdescent <elshanielird@gmail.com>
2024-02-02 16:26:33 +0100

    Client List changes

--- resources/views/clientlist.blade.php
+++ resources/views/clientlist.blade.php
@@ -106,9 +106,15 @@
   <!-- component -->
 
     <!-- CLIENTS TABLE -->
+    
 
     <!-- component -->
     <section class="container mx-auto p-6 ">
+        <div class="mb-2 flex justify-start items-center">
+     
+        <button class="btn btn-sm rounded-full " onclick="my_modal_add.showModal()"><svg viewBox="0 0 24 24"class="h-5" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 18L17 18M17 18L14 18M17 18V15M17 18V21M11 21H4C4 17.134 7.13401 14 11 14C11.695 14 12.3663 14.1013 13 14.2899M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#00f7ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> <span class="text-cyan-400"> Add Client</span> </button>
+
+    </div>
         <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
           <div class="w-full overflow-x-auto">
             <table class="w-full">
@@ -117,9 +123,10 @@
                   <th class="px-4 py-3">Name</th>
                   <th class="px-4 py-3">ID</th>
                   <th class="px-4 py-3">Case</th>
-                  <th class="px-4 py-3"></th>
-                  <th class="px-4 py-3"></th>
-                  <td class=" py-3 text-sm"><button class="btn btn-sm rounded-full" onclick="my_modal_add.showModal()"><svg viewBox="0 0 24 24"class="h-5" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 18L17 18M17 18L14 18M17 18V15M17 18V21M11 21H4C4 17.134 7.13401 14 11 14C11.695 14 12.3663 14.1013 13 14.2899M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#00f7ff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button></td>
+                  <th colspan="3"></th>
+
+                 
+
                 </tr>
               </thead>
               <tbody class="bg-neutral-800">
