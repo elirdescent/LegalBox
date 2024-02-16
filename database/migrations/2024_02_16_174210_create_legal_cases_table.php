62add0360c84b6733917c010bf081870f1bba836
BajramAmeti <ba61296@ubt-uni.net>
2024-02-16 18:52:50 +0100

    Added legal_cases table columns

--- database/migrations/2024_02_16_174210_create_legal_cases_table.php
+++ database/migrations/2024_02_16_174210_create_legal_cases_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('legal_cases', function (Blueprint $table) {
             $table->id();
+            $table->integer('case_id');
+            $table->string('title');
             $table->timestamps();
         });
     }
