7e0f64f1317b3437c89a54c6798b88a90cbb0bcc
BajramAmeti <ba61296@ubt-uni.net>
2024-02-16 18:50:08 +0100

    Added API Methods for LegalCaseController

--- app/Http/Controllers/LegalCaseController.php
+++ app/Http/Controllers/LegalCaseController.php
@@ -1,10 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\LegalCase;
 
-class LegalCaseController extends Controller
+class ClientController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+    }
+    
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return LegalCase::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+     
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+       
+    }
+
+
+   /**
+     * @param string $name
+     * @return \Illuminate\Http\Response 
+     */
+    public function search($name)
+    {
+     
+
+    }
 }
+
